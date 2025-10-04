eeac961a67ffe1b346ae0aae9099bbf267b0f02e
dhawall-s <sainidhawal755@gmail.com>
2025-10-04 16:13:59 +0530

    first commit

---
+++ Javascript/index.js
@@ -0,0 +1,21 @@
+//variable kaise banate hain
+let name='Dhawal';
+let age=20
+age=age+1
+console.log(name , age )
+
+//data change nhi hoga const mein
+const salary=3000000000000
+console.log(salary)
+
+//older version of declaring datatype
+//use nahi karte kyunki var ke variable ko apan do baar declare kar dete hain
+//and do not follow the proper scoping of the variables
+var a=10
+a=20
+console.log(a)
+
+//Mostly let and const are used
+
+
+
