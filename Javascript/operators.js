f3275ea7b27b606855bf3c5e8cd5ac69a4596dd4
dhawall-s <sainidhawal755@gmail.com>
2025-10-04 17:24:10 +0530

    first commit

--- Javascript/operators.js
+++ Javascript/operators.js
@@ -1,10 +1,41 @@
-console.log(2+3)
-console.log(2-5)
-console.log(2/1)
-console.log(2*5)
-console.log(5%2)
+// console.log(2 + 3);
+// console.log(2 - 5);
+// console.log(2 / 1);
+// console.log(2 * 5);
+// console.log(5 % 2);
 
-console.log(5**2)
-console.log(5**3)
-console.log(5**4)
+// console.log(5 ** 2);
+// console.log(5 ** 3);
+// console.log(5 ** 4);
 
+// //Assignment Operator
+
+// let x1 = 10;
+// let y1 = 20;
+// // x=x+y
+// // x=x*y
+// // x=y/x
+// x1 += y1;
+// console.log(x1);
+
+// //comparison operator
+
+let x2 = 10;
+let y2 = 20;
+console.log(x2 > y2);
+console.log(x2 < y2);
+console.log(x2 <= 10);
+console.log(y2 >= 20); 
+
+//type conversion
+let z='10'
+let a= Number(z)
+console.log(typeof(a))
+console.log(a)
+
+
+// difference b/w '=='  and '==='
+let x3=20
+let y3='20'
+console.log(x3==y3) //string ko number mein convert kar deta hain jabki ek variable string hoti hain phir bhi
+console.log(x3===y3) // yeh pehle hi datatype check karta hain aur phir uske baad comparison karta hain
