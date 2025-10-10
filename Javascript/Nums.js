3dc0799ca29ec8a551e1494d1806d7794c6be51a
dhawall-s <sainidhawal755@gmail.com>
2025-10-10 09:31:30 +0530

    first commit

---
+++ Javascript/Nums.js
@@ -0,0 +1,17 @@
+// let a = 10;
+
+// console.log(a);
+
+// let b = 345.8909;
+// console.log(b.toFixed(1)); // with roundoffs
+// console.log(b);
+
+// console.log(b.toPrecision(4));
+
+// console.log(typeof b.toString());
+
+let n = new Number(20);
+console.log(n);
+console.log(typeof n);
+
+
