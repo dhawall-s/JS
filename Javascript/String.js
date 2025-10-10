2ee8d061c506341e7b7acbf15a22dea93e6cc501
dhawall-s <sainidhawal755@gmail.com>
2025-10-10 10:07:51 +0530

    first commit

--- Javascript/String.js
+++ Javascript/String.js
@@ -1,7 +1,18 @@
-const str1="Dhawal"
-const str2='Saini'
-const str3=`Raja`
-const str5=20
-const str4=`Diwali is on ${str5}`
-console.log(str4)
-console.log(str1,str2,str3)
+const str1 = "Dhawal";
+const str2 = "Saini";
+const str3 = `Raja`;
+const str5 = 20;
+const str4 = `Diwali is on ${str5}`;
+console.log(str4);
+console.log(str1, str2, str3);
+
+const str = `Hello DSA Army`;
+console.log(str.length);
+
+console.log(str.toLowerCase());
+
+console.log(str.toUpperCase());
+
+console.log(str.indexOf("Ar"));
+
+console.log(str.includes("Army"));
