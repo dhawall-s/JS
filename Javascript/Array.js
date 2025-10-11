9d1550c928879e08b4440bcbb307c8c519116190
dhawall-s <sainidhawal755@gmail.com>
2025-10-11 09:43:48 +0530

    first commit

--- Javascript/Array.js
+++ Javascript/Array.js
@@ -33,12 +33,36 @@ let arr2 = [10, 30, 50, 90, 11];
 //   console.log(num);
 // }
 
-let arr3=[10,20,30,40,50]
-arr2=arr3
-arr2[1]=999
-console.log(arr2)
-console.log(arr3)
+let arr3 = [10, 20, 30, 40, 50];
+arr2 = arr3;
+arr2[1] = 999;
+console.log(arr2);
+console.log(arr3);
 
+//objects : copy by referenct hote hain
+//primitive datatypes : copy by value hote hain nayi copy banti hain memory mein
+
+//const ka address change nhi ho sakta
 // const a=[10,20,30]
 // a=[20]
 // console.log(a)
+
+let a3 = [10, 30, 50, 90, 11];
+console.log(a3.slice(1, 3)); //last wala not included no change in original array
+console.log(a3);
+console.log(a3.splice(1, 3, "Dhawal", 19)); // 1st element se 3 elements ko trim out karwa do original array mein change hoga
+console.log(a3);
+
+const name = ["dhawal", "saini"];
+const skills = ["PSS"];
+//name.push(skills);
+//console.log(name);
+//console.log(skills)
+const arr4 = name.concat(skills);
+console.log(arr4);
+
+const arr5 = [...name, ...arr2, ...skills];
+console.log(arr5);
+
+const naam = ["Dhakad Devank", "Rockstar Dhawal", "Agent Ashu", "Raid Machine", "Naveen"];
+console.log(naam.join(","));
